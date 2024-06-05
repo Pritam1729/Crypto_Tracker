1143864780e74b9179cf376e3106497b914dc1db
Pritam1729 <pes1202102554@pesu.pes.edu>
2024-06-06 00:46:14 +0530

    reverted

--- src/components/LandingPage/MainCompoment/index.js
+++ src/components/LandingPage/MainCompoment/index.js
@@ -16,9 +16,9 @@ function MainComponent() {
                     Track Crypto
             </motion.h1>
             <motion.h1 className = "real-time-heading" initial ={{opacity:0,y:50}} animate={{opacity:1,y:0}} transition={{transition:1, delay:0.5}}>Real Time</motion.h1>
-            {/* <motion.p className = "info-text" initial = {{opacity:0,y:50}} animate ={{opacity:1,y:0}} transition={{transition:1, delay:0.75}}>
+            <motion.p className = "info-text" initial = {{opacity:0,y:50}} animate ={{opacity:1,y:0}} transition={{transition:1, delay:0.75}}>
                 Track crypto through a public api in real time. Visit the dashboard to do so!
-            </motion.p> */}
+            </motion.p>
             <motion.div className= "btn-flex" initial={{opacity:0,x:50}} animate={{opacity:1,x:0}} transition={{transition:1,delay:1}}>
                 <Button text = {"Dashboard"} />
                 <Button text = {"Share"} outlined={true} />
